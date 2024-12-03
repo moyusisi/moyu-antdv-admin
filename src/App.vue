ed04fc54c4d49f4aa36d516d0a288bf7912aa685
shisong <rgkjhshi@gmail.com>
2024-12-03 23:54:32 +0800

    router-view

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,20 @@
+<template>
+  <a-config-provider>
+    <div id="app" class="app">
+      <router-view />
+    </div>
+  </a-config-provider>
+</template>
+
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
-
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.app {
+  /* overflow: hidden; */
+  overflow: auto;
+  border: none;
+  /* background: #F6F7F8; */
 }
 </style>
