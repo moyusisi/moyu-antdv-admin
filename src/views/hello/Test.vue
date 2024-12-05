66336d7bb461f8dcf5ce9aae5e73140923e3b19f
shisong <rgkjhshi@gmail.com>
2024-12-05 11:43:59 +0800

    Create Test.vue

---
+++ src/views/hello/Test.vue
@@ -0,0 +1,18 @@
+<template>
+  <a-layout>
+    <!--  左侧侧边栏  -->
+    <Sidebar/>
+    <!-- 右侧布局 -->
+    <a-layout>
+    </a-layout>
+  </a-layout>
+</template>
+
+<script setup lang="ts">
+import Sidebar from "@/layout/SideBar/index.vue";
+
+</script>
+
+<style scoped>
+
+</style>
