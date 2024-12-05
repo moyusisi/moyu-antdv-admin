e55a447a8cd9749770421d33adb696fac779bcaa
shisong <rgkjhshi@gmail.com>
2024-12-05 11:43:53 +0800

    Update ClassicLayout.vue

--- src/layout/ClassicLayout.vue
+++ src/layout/ClassicLayout.vue
@@ -11,25 +11,6 @@
 <script setup lang="ts">
 import Sidebar from "@/layout/SideBar/index.vue";
 
-const props = defineProps({
-  layout: { type: String }, // 布局信息
-  menuCollapse: { type: Boolean }, // 菜单是否折叠
-  sysBaseConfig: { type: Object },
-  openKeys: { type: Array },
-  selectedKeys: { type: Array },
-  menuList: { type: Array }, // 菜单
-  breadcrumbOpen: { type: Boolean }, //面包屑
-  tagsViewOpen: { type: Boolean },
-  moduleMenuShow: { type: Boolean }
-})
-
-const emit = defineEmits(['onSelect', 'onOpenChange', 'switchModule', 'menuCollapseClick'])
-const onSelect = (obj) => {
-  emit('onSelect', obj)
-}
-const onOpenChange = (keys) => {
-  emit('onOpenChange', keys)
-}
 </script>
 
 <style scoped>
