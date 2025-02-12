d9b272f1d27875230d2afdbe3f685f86e22b76b0
shisong <rgkjhshi@gmail.com>
2025-02-12 17:23:32 +0800

    move

--- src/views/hello/HelloWorld.vue
+++ src/views/hello/HelloWorld.vue
@@ -7,7 +7,7 @@ const count = ref(0)
 <template>
   <div>
     <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+      <img src="/img/logo.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="@/assets/vue.svg" class="logo vue" alt="Vue logo" />
