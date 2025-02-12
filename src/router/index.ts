92c0e3cebf89166ebdf02761b0e445cf751112b0
shisong <rgkjhshi@gmail.com>
2025-02-12 16:45:31 +0800

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 // 公共路由
 export const constantRoutes: RouteRecordRaw[] = [
   {
-    path: '/hello',
+    path: '/',
     name: 'Hello',
     component: () => import('@/views/hello/HelloWorld.vue'),
     meta: { title: "Hello", hidden: true }
