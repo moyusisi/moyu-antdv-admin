50a04c4a98b14892804b0f90d45a56a33534bcb0
shisong <rgkjhshi@gmail.com>
2025-02-12 15:52:17 +0800

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,53 +1,12 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import ClassicLayout from '@/layout/ClassicLayout.vue'
 
 // 公共路由
 export const constantRoutes: RouteRecordRaw[] = [
   {
-    path: '/test',
-    component: () => import('@/views/hello/Test.vue'),
-    meta: { hidden: true }
-  },
-
-  {
-    path: '/',
-    name: '/',
-    component: ClassicLayout,
-    redirect: '/home',
-    children: [
-      {
-        path: 'home',
-        component: () => import('@/views/hello/HelloWorld.vue'),
-        name: 'Home',
-        meta: {
-          title: 'Home',
-          icon: 'homepage',
-          affix: true,
-          keepAlive: true,
-          alwaysShow: false
-        }
-      }
-    ]
-  },
-  {
-    path: '/layout',
-    name: 'ClassicLayout',
-    component: ClassicLayout,
-    redirect: '/layout/hello',
-    children: [
-      {
-        path: 'hello',
-        component: () => import('@/views/hello/HelloWorld.vue'),
-        name: 'Hello',
-        meta: {
-          title: 'Hello',
-          icon: 'homepage',
-          affix: true,
-          keepAlive: true,
-          alwaysShow: false
-        }
-      }
-    ]
+    path: '/hello',
+    name: 'Hello',
+    component: () => import('@/views/hello/HelloWorld.vue'),
+    meta: { title: "Hello", hidden: true }
   }
 ]
 
