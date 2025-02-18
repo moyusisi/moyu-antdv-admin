220da6ef971116cae7fe6d7b98b2fba9a84ed950
shisong <rgkjhshi@gmail.com>
2025-02-18 15:20:12 +0800

    views

---
+++ src/views/sys/role/index.vue
@@ -0,0 +1,271 @@
+<template>
+	<a-card size="small">
+		<a-form ref="searchFormRef" :model="searchFormData">
+			<a-row :gutter="24">
+				<a-col :span="8">
+					<a-form-item name="searchKey" label="名称关键词">
+						<a-input v-model:value="searchFormData.searchKey" placeholder="请输入关键词" allowClear />
+					</a-form-item>
+				</a-col>
+				<a-col :span="6">
+					<a-form-item label="使用状态" name="status">
+						<a-select v-model:value="searchFormData.status" placeholder="请选择状态" :options="statusOptions" allowClear />
+					</a-form-item>
+				</a-col>
+				<a-col :span="8">
+					<a-space>
+						<a-button type="primary" :icon="h(SearchOutlined)" @click="tableRef.refresh(true)">查询</a-button>
+						<a-button :icon="h(RedoOutlined)" @click="reset">重置</a-button>
+					</a-space>
+				</a-col>
+			</a-row>
+		</a-form>
+	</a-card>
+	<a-card size="small">
+		<s-table
+			ref="tableRef"
+			:columns="columns"
+			:data="loadData"
+			:alert="options.alert.show"
+			bordered
+			:row-key="(record) => record.id"
+			:row-selection="options.rowSelection"
+			:tool-config="toolConfig"
+		>
+			<template #operator>
+				<a-space>
+					<a-button type="primary" :icon="h(PlusOutlined)" @click="addFormRef.onOpen(module)">新增角色</a-button>
+					<BatchDeleteButton icon="DeleteOutlined" :selectedRowKeys="selectedRowKeys" @batchDelete="deleteBatchRole" />
+				</a-space>
+			</template>
+			<template #bodyCell="{ column, record }">
+				<template v-if="column.dataIndex === 'code'">
+					<a-tag v-if="record.code" :bordered="false">{{ record.code }}</a-tag>
+				</template>
+				<template v-if="column.dataIndex === 'status'">
+					<a-tag v-if="record.status === 0" color="green">正常</a-tag>
+					<a-tag v-else>已停用</a-tag>
+				</template>
+				<template v-if="column.dataIndex === 'action'">
+					<a-space>
+						<a @click="grantMenuFormRef.onOpen(record)">授权</a>
+						<a-divider type="vertical" />
+						<a-tooltip title="编辑">
+							<a @click="editFormRef.onOpen(record)"><FormOutlined /></a>
+						</a-tooltip>
+						<a-divider type="vertical" />
+						<a-tooltip title="删除">
+							<a-popconfirm title="确定要删除吗？" @confirm="deleteRole(record)">
+								<a style="color:#FF4D4F;"><DeleteOutlined/></a>
+							</a-popconfirm>
+						</a-tooltip>
+					</a-space>
+<!--					<a-dropdown>-->
+<!--						<a class="ant-dropdown-link">-->
+<!--							授权-->
+<!--							<DownOutlined />-->
+<!--						</a>-->
+<!--						<template #overlay>-->
+<!--							<a-menu>-->
+<!--								<a-menu-item>-->
+<!--									<a @click="grantResourceFormRef.onOpen(record)">授权资源</a>-->
+<!--								</a-menu-item>-->
+<!--								<a-menu-item>-->
+<!--									<a @click="grantMobileResourceFormRef.onOpen(record)">授权移动端资源</a>-->
+<!--								</a-menu-item>-->
+<!--								<a-menu-item>-->
+<!--									<a @click="grantPermissionFormRef.onOpen(record)">授权权限</a>-->
+<!--								</a-menu-item>-->
+<!--								<a-menu-item>-->
+<!--									<a @click="openRoleUserSelector(record)">授权用户</a>-->
+<!--								</a-menu-item>-->
+<!--							</a-menu>-->
+<!--						</template>-->
+<!--					</a-dropdown>-->
+				</template>
+			</template>
+		</s-table>
+	</a-card>
+	<grant-menu-form ref="grantMenuFormRef" @successful="tableRef.refresh()" />
+	<grantResourceForm ref="grantResourceFormRef" @successful="tableRef.refresh()" />
+	<grantMobileResourceForm ref="grantMobileResourceFormRef" @successful="tableRef.refresh()" />
+	<grantPermissionForm ref="grantPermissionFormRef" @successful="tableRef.refresh()" />
+	<Form ref="formRef" @successful="tableRef.refresh()" />
+	<AddForm ref="addFormRef" @successful="tableRef.refresh()" />
+	<EditForm ref="editFormRef" @successful="tableRef.refresh()" />
+	<xn-user-selector
+		ref="userSelectorPlusRef"
+		:org-tree-api="selectorApiFunction.orgTreeApi"
+		:user-page-api="selectorApiFunction.userPageApi"
+		data-type="object"
+		:user-show="false"
+		@onBack="userCallBack"
+	/>
+</template>
+
+<script setup>
+	import roleApi from '@/api/sys/roleApi'
+
+	import { h } from "vue"
+	import { PlusOutlined, RedoOutlined, SearchOutlined } from "@ant-design/icons-vue"
+	import GrantResourceForm from './grantResourceForm.vue'
+	import GrantMobileResourceForm from './grantMobileResourceForm.vue'
+	import GrantPermissionForm from './grantPermissionForm.vue'
+	import Form from './form.vue'
+	import AddForm from "./addForm.vue";
+	import EditForm from "./editForm.vue";
+	import GrantMenuForm from "./grantMenuForm.vue";
+	import { message } from "ant-design-vue";
+	import BatchDeleteButton from "@/components/BatchDeleteButton/index.vue";
+
+	const columns = [
+		{
+			title: '角色名称',
+			dataIndex: 'name',
+			resizable: true,
+			width: 150
+		},
+		{
+			title: '唯一编码',
+			dataIndex: 'code',
+			resizable: true,
+			width: 200
+		},
+		{
+			title: '排序',
+			dataIndex: 'sortNum',
+			sorter: true,
+			align: 'center',
+			width: 100
+		},
+		{
+			title: '状态',
+			dataIndex: 'status',
+			align: 'center',
+			width: 100
+		},
+		{
+			title: '创建时间',
+			dataIndex: 'createTime',
+			align: 'center',
+			width: 160
+		},
+		{
+			title: '更新时间',
+			dataIndex: 'updateTime',
+			align: 'center',
+			width: 160
+		},
+		{
+			title: '操作',
+			dataIndex: 'action',
+			align: 'center',
+			resizable: true,
+			width: 150
+		}
+	]
+	const selectedRowKeys = ref([])
+	// 使用状态options（0正常 1停用）
+	const statusOptions = [
+		{ label: "正常", value: 0 },
+		{ label: "已停用", value: 1 }
+	]
+	// 列表选择配置
+	const options = {
+		alert: {
+			show: false,
+			clear: () => {
+				selectedRowKeys.value = ref([])
+			}
+		},
+		rowSelection: {
+			onChange: (selectedRowKey, selectedRows) => {
+				selectedRowKeys.value = selectedRowKey
+			}
+		}
+	}
+	// 定义tableDOM
+	const tableRef = ref()
+	const formRef = ref()
+	const addFormRef = ref()
+	const editFormRef = ref()
+	const module = ref()
+	const toolConfig = { refresh: true, height: true, columnSetting: false, striped: false }
+	const grantMenuFormRef = ref()
+	const grantResourceFormRef = ref()
+	const grantMobileResourceFormRef = ref()
+	const grantPermissionFormRef = ref()
+	const userSelectorPlusRef = ref()
+	const searchFormRef = ref()
+	const searchFormData = ref({})
+	// 记录数据
+	const recordCacheData = ref({})
+
+	// 表格查询 返回 Promise 对象
+	const loadData = (parameter) => {
+		let param = Object.assign(parameter, searchFormData.value)
+		return roleApi.rolePage(param).then((res) => {
+			return res.data
+		})
+	}
+	// 重置
+	const reset = () => {
+		searchFormRef.value.resetFields()
+		tableRef.value.refresh(true)
+	}
+	// 删除
+	const deleteRole = (record) => {
+		let data = { ids: [record.id] }
+		roleApi.deleteRole(data).then((res) => {
+			message.success(res.message)
+			tableRef.value.refresh(true)
+		})
+	}
+	// 批量删除
+	const deleteBatchRole = (params) => {
+		let data = { ids: selectedRowKeys.value }
+		roleApi.deleteRole(data).then((res) => {
+			message.success(res.message)
+			tableRef.value.clearRefreshSelected()
+		})
+	}
+	// 打开用户选择器
+	const openRoleUserSelector = (record) => {
+		// 打开人员选择器的时候，缓存一个记录数据
+		recordCacheData.value = record
+		// 查询接口，查到这个角色是多少个用户都有它
+		const param = {
+			id: record.id
+		}
+		roleApi.roleOwnUser(param).then((data) => {
+			userSelectorPlusRef.value.showUserPlusModal(data)
+		})
+	}
+	// 人员选择器回调
+	const userCallBack = (value) => {
+		const param = {
+			id: recordCacheData.value.id,
+			grantInfoList: value
+		}
+		roleApi.roleGrantUser(param).then(() => {})
+	}
+	// 传递设计器需要的API
+	const selectorApiFunction = {
+		orgTreeApi: (param) => {
+			return roleApi.roleOrgTreeSelector(param).then((data) => {
+				return Promise.resolve(data)
+			})
+		},
+		userPageApi: (param) => {
+			return roleApi.roleUserSelector(param).then((data) => {
+				return Promise.resolve(data)
+			})
+		}
+	}
+</script>
+
+<style scoped>
+	.ant-form-item {
+		margin-bottom: 0 !important;
+	}
+</style>
---
+++ src/views/sys/user/index.vue
@@ -0,0 +1,363 @@
+<template>
+	<a-row :gutter="8">
+		<!-- 左侧组织树 -->
+		<a-col :span="5">
+			<OrgTree @onSelect="treeSelect"></OrgTree>
+		</a-col>
+		<!-- 右侧内容 -->
+		<a-col :span="19">
+			<a-card size="small">
+				<a-form ref="searchFormRef" :model="searchFormData">
+					<a-row :gutter="24">
+						<a-col :span="8">
+							<a-form-item name="searchKey" label="关键词">
+								<a-input v-model:value="searchFormData.searchKey" placeholder="请输入姓名或关键词" allowClear />
+							</a-form-item>
+						</a-col>
+						<a-col :span="6">
+							<a-form-item label="用户状态" name="status">
+								<a-select v-model:value="searchFormData.status" placeholder="请选择状态" :options="statusOptions" allowClear />
+							</a-form-item>
+						</a-col>
+						<a-col :span="8">
+							<a-space>
+								<a-button type="primary" :icon="h(SearchOutlined)" @click="tableRef.refresh(true)">查询</a-button>
+								<a-button :icon="h(RedoOutlined)" @click="reset">重置</a-button>
+							</a-space>
+						</a-col>
+					</a-row>
+				</a-form>
+			</a-card>
+			<a-card size="small">
+				<s-table
+					ref="tableRef"
+					:columns="columns"
+					:data="loadTableData"
+					:scroll="{ x: 1200 }"
+					bordered
+					:alert="options.alert.show"
+					:row-key="(record) => record.id"
+					:row-selection="options.rowSelection"
+					:tool-config="toolConfig"
+				>
+					<template #operator>
+						<a-space>
+							<a-button type="primary" :icon="h(PlusOutlined)" @click="addFormRef.onOpen(searchFormData.orgCode)">新增用户</a-button>
+							<a-button @click="ImpExpRef.onOpen()">
+								<template #icon><import-outlined /></template>
+								<span>{{ $t('common.imports') }}</span>
+							</a-button>
+							<a-button @click="exportBatchUserVerify">
+								<template #icon><export-outlined /></template>
+								{{ $t('user.batchExportButton') }}
+							</a-button>
+							<BatchDeleteButton icon="DeleteOutlined" :selectedRowKeys="selectedRowKeys" @batchDelete="batchDeleteUser" />
+						</a-space>
+					</template>
+					<template #bodyCell="{ column, record }">
+						<template v-if="column.dataIndex === 'gender'">
+							<a-tag v-if="record.gender === 1" color="blue">男</a-tag>
+							<a-tag v-else-if="record.gender === 2" color="pink">女</a-tag>
+							<a-tag v-else>未知</a-tag>
+						</template>
+						<template v-if="column.dataIndex === 'status'">
+							<a-tag v-if="record.status === 0" color="green">正常</a-tag>
+							<a-tag v-else>已停用</a-tag>
+						</template>
+						<template v-if="column.dataIndex === 'action'">
+							<a-space>
+								<a-tooltip title="编辑">
+									<a @click="editFormRef.onOpen(record)"><FormOutlined /></a>
+								</a-tooltip>
+								<a-divider type="vertical" />
+								<a-tooltip title="删除">
+									<a-popconfirm title="确定要删除吗？" @confirm="deleteUser(record)">
+										<a style="color:#FF4D4F;"><DeleteOutlined/></a>
+									</a-popconfirm>
+								</a-tooltip>
+								<a-divider type="vertical" />
+								<a-tooltip title="重置密码">
+								  <a-popconfirm title="确定要重置吗？" @confirm="resetPassword(record)">
+									<a style="color:darkorange;"><LockOutlined/></a>
+								  </a-popconfirm>
+								</a-tooltip>
+								<a-divider type="vertical" />
+							</a-space>
+							<a-dropdown>
+								<a class="ant-dropdown-link">
+									{{ $t('common.more') }}
+									<DownOutlined />
+								</a>
+								<template #overlay>
+									<a-menu>
+										<a-menu-item>
+											<a-popconfirm
+												:title="$t('user.popconfirmResatUserPwd')"
+												placement="topRight"
+												@confirm="resetPassword(record)"
+											>
+												<a>{{ $t('user.resetPassword') }}</a>
+											</a-popconfirm>
+										</a-menu-item>
+										<a-menu-item>
+											<a @click="selectRole(record)">{{ $t('user.grantRole') }}</a>
+										</a-menu-item>
+										<a-menu-item>
+											<a @click="grantResourceFormRef.onOpen(record)">{{ $t('user.grantResource') }}</a>
+										</a-menu-item>
+										<a-menu-item>
+											<a @click="grantPermissionFormRef.onOpen(record)">{{ $t('user.grantPermission') }}</a>
+										</a-menu-item>
+										<a-menu-item>
+											<a @click="exportUserInfo(record)">{{ $t('user.exportUserInfo') }}</a>
+										</a-menu-item>
+									</a-menu>
+								</template>
+							</a-dropdown>
+						</template>
+					</template>
+				</s-table>
+			</a-card>
+		</a-col>
+	</a-row>
+	<xn-role-selector
+		ref="RoleSelectorPlusRef"
+		:org-tree-api="selectorApiFunction.orgTreeApi"
+		:role-page-api="selectorApiFunction.rolePageApi"
+		:add-show="false"
+		:show="false"
+		:role-global="true"
+		@onBack="roleBack"
+	/>
+	<ImpExp ref="ImpExpRef" />
+	<grantResourceForm ref="grantResourceFormRef" @successful="tableRef.refresh()" />
+	<grantPermissionForm ref="grantPermissionFormRef" @successful="tableRef.refresh()" />
+	<EditForm ref="editFormRef" @successful="tableRef.refresh()" />
+	<AddForm ref="addFormRef" @successful="tableRef.refresh()" />
+</template>
+
+<script setup>
+	import userApi from '@/api/sys/userApi'
+	import orgApi from '@/api/sys/orgApi'
+
+	import { h } from "vue";
+	import { message, Empty } from 'ant-design-vue'
+	import { SearchOutlined, RedoOutlined, PlusOutlined } from "@ant-design/icons-vue";
+	import downloadUtil from '@/utils/downloadUtil'
+	import ImpExp from './impExp.vue'
+	import GrantResourceForm from './grantResourceForm.vue'
+	import GrantPermissionForm from './grantPermissionForm.vue'
+	import AddForm from './addForm.vue'
+	import EditForm from "./editForm.vue"
+	import OrgTree from "../components/orgTree.vue";
+	import BatchDeleteButton from "@/components/BatchDeleteButton/index.vue";
+
+	const columns = [
+		{
+			title: '账号',
+			dataIndex: 'account',
+			resizable: true,
+			width: 200,
+			ellipsis: true
+		},
+		{
+			title: '姓名',
+			dataIndex: 'name',
+			resizable: true,
+			width: 150
+		},
+		{
+			title: '性别',
+			dataIndex: 'gender',
+			align: 'center',
+			width: 80
+		},
+		{
+			title: '手机',
+			dataIndex: 'phone',
+			align: 'center',
+			width: 150
+		},
+		{
+			title: '组织机构',
+			dataIndex: 'orgName',
+			resizable: true,
+			width: 200,
+			ellipsis: true
+		},
+		{
+			title: '状态',
+			dataIndex: 'status',
+			align: 'center',
+			width: 80
+		},
+		{
+			title: '操作',
+			dataIndex: 'action',
+			align: 'center',
+			resizable: true,
+			width: 200
+		}
+	]
+	const selectedRowKeys = ref([])
+	// 列表选择配置
+	const options = {
+		alert: {
+			show: false,
+			clear: () => {
+				selectedRowKeys.value = ref([])
+			}
+		},
+		rowSelection: {
+			onChange: (selectedRowKey, selectedRows) => {
+				selectedRowKeys.value = selectedRowKey
+			}
+		}
+	}
+	// 使用状态options（0正常 1停用）
+	const statusOptions = [
+		{ label: "正常", value: 0 },
+		{ label: "已停用", value: 1 }
+	]
+	// 定义tableDOM
+	const tableRef = ref()
+	const toolConfig = { refresh: true, height: true, columnSetting: false, striped: false }
+	const addFormRef = ref()
+	const editFormRef = ref()
+	const searchFormRef = ref()
+	const searchFormData = ref({})
+
+	const RoleSelectorPlusRef = ref()
+	const selectedRecord = ref({})
+	const loading = ref(false)
+	const ImpExpRef = ref()
+	const grantResourceFormRef = ref()
+	const grantPermissionFormRef = ref()
+
+	// 表格查询 返回 Promise 对象
+	const loadTableData = (parameter) => {
+		return userApi.userPage(Object.assign(parameter, searchFormData.value)).then((res) => {
+			return res.data
+		})
+	}
+	// 重置
+	const reset = () => {
+		searchFormRef.value.resetFields()
+		tableRef.value.refresh(true)
+	}
+	// 点击树查询
+	const treeSelect = (selectedKeys) => {
+		if (selectedKeys.length > 0) {
+			searchFormData.value.orgCode = selectedKeys.toString()
+		} else {
+			delete searchFormData.value.orgCode
+		}
+		tableRef.value.refresh(true)
+	}
+	// 删除用户
+	const deleteUser = (record) => {
+		let data = { ids: [record.id] }
+		userApi.deleteUser(data).then((res) => {
+			message.success(res.message)
+			tableRef.value.refresh(true)
+		})
+	}
+	// 批量删除
+	const batchDeleteUser = (params) => {
+		let data = { ids: selectedRowKeys.value }
+		userApi.deleteUser(data).then((res) => {
+			message.success(res.message)
+			tableRef.value.clearRefreshSelected()
+		})
+	}
+	// 批量导出校验并加参数
+	const exportBatchUserVerify = () => {
+		if ((selectedRowKeys.value.length < 1) & !searchFormData.value.searchKey & !searchFormData.value.userStatus) {
+			message.warning('请输入查询条件或勾选要导出的信息')
+		}
+		if (selectedRowKeys.value.length > 0) {
+			const params = {
+				userIds: selectedRowKeys.value
+					.map((m) => {
+						return m
+					})
+					.join()
+			}
+			exportBatchUser(params)
+			return
+		}
+		if (searchFormData.value.searchKey || searchFormData.value.userStatus) {
+			const params = {
+				searchKey: searchFormData.value.searchKey,
+				userStatus: searchFormData.value.userStatus
+			}
+			exportBatchUser(params)
+		}
+	}
+	// 批量导出
+	const exportBatchUser = (params) => {
+		userApi.userExport(params).then((res) => {
+			downloadUtil.resultDownload(res)
+			tableRef.value.clearSelected()
+		})
+	}
+	// 打开角色选择器
+	const selectRole = (record) => {
+		selectedRecord.value = record
+		// 查询到已有角色，并转为ids的格式，给角色选择器
+		const param = {
+			id: record.id
+		}
+		userApi.userOwnRole(param).then((data) => {
+			RoleSelectorPlusRef.value.showModel(data)
+		})
+	}
+	// 角色选择回调
+	const roleBack = (value) => {
+		let params = {
+			id: selectedRecord.value.id,
+			roleIdList: []
+		}
+		if (value.length > 0) {
+			value.forEach((item) => {
+				params.roleIdList.push(item)
+			})
+		}
+		userApi.grantRole(params).then(() => {})
+	}
+	// 重置用户密码
+	const resetPassword = (record) => {
+		let data = { ids: [record.id] }
+		userApi.resetPassword(data).then((res) => {
+			message.success(res.message)
+		})
+	}
+	// 导出用户信息
+	const exportUserInfo = (record) => {
+		const params = {
+			id: record.id
+		}
+		userApi.userExportUserInfo(params).then((res) => {
+			downloadUtil.resultDownload(res)
+		})
+	}
+	// 传递设计器需要的API
+	const selectorApiFunction = {
+		orgTreeApi: (param) => {
+			return userApi.userOrgTreeSelector(param).then((data) => {
+				return Promise.resolve(data)
+			})
+		},
+		rolePageApi: (param) => {
+			return userApi.userRoleSelector(param).then((data) => {
+				return Promise.resolve(data)
+			})
+		}
+	}
+</script>
+
+<style scoped>
+	.ant-form-item {
+		margin-bottom: 0 !important;
+	}
+</style>
