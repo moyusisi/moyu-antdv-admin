845c4947523ed29e22ee51be841506221f9adbef
shisong <rgkjhshi@gmail.com>
2025-09-27 22:15:15 +0800

    tab替换

--- src/views/dev/gen/index.vue
+++ src/views/dev/gen/index.vue
@@ -12,7 +12,7 @@
       </a-form-item>
     </a-form>
   </a-card>
-	<a-card size="small">
+    <a-card size="small">
     <STable
         ref="tableRef"
         :columns="columns"
@@ -23,41 +23,41 @@
         :row-selection="options.rowSelection"
         :tool-config="toolConfig"
     >
-			<template #operator>
+            <template #operator>
         <a-space>
           <a-button type="primary" :icon="h(PlusOutlined)" @click="xx.onOpen(module)">从SQL导入</a-button>
           <a-button type="primary" :icon="h(CloudUploadOutlined)" @click="importFormRef.onOpen()">导入</a-button>
           <BatchDeleteButton icon="DeleteOutlined" :selectedRowKeys="selectedRowKeys" @batchDelete="batchDelete" />
         </a-space>
-			</template>
-			<template #bodyCell="{ column, record, index }">
+            </template>
+            <template #bodyCell="{ column, record, index }">
         <template v-if="column.dataIndex === 'index'">
           <span>{{ index + 1 }}</span>
         </template>
-				<template v-if="column.dataIndex === 'action'">
-					<a-space>
+                <template v-if="column.dataIndex === 'action'">
+                    <a-space>
             <a-tooltip title="预览代码">
               <a style="color:#53C61D;" @click="previewRef.onOpen(record)">预览</a>
             </a-tooltip>
             <a-tooltip title="下载生成的代码">
               <a style="color:#1980FF;" @click="configFormRef.onOpen(record)">生成</a>
             </a-tooltip>
-						<a-tooltip title="修改配置">
+                        <a-tooltip title="修改配置">
               <a @click="configFormRef.onOpen(record)">修改</a>
-						</a-tooltip>
+                        </a-tooltip>
             <a-tooltip title="根据表结构同步默认配置">
               <a @click="syncTable(record)">同步</a>
             </a-tooltip>
-						<a-tooltip title="删除配置">
-							<a-popconfirm title="确定要删除配置吗？" @confirm="deleteConfig(record)">
-								<a style="color:#FF4D4F;">删除</a>
-							</a-popconfirm>
-						</a-tooltip>
-					</a-space>
-				</template>
-			</template>
-		</STable>
-	</a-card>
+                        <a-tooltip title="删除配置">
+                            <a-popconfirm title="确定要删除配置吗？" @confirm="deleteConfig(record)">
+                                <a style="color:#FF4D4F;">删除</a>
+                            </a-popconfirm>
+                        </a-tooltip>
+                    </a-space>
+                </template>
+            </template>
+        </STable>
+    </a-card>
   <ImportForm ref="importFormRef" @successful="tableRef.refresh(true)" />
   <ConfigForm ref="configFormRef" @successful="tableRef.refresh(true)" />
   <previewCode ref="previewRef" />
@@ -66,36 +66,36 @@
 <script setup>
   import codegenApi from '@/api/dev/codegenApi'
 
-	import { h } from "vue"
+    import { h } from "vue"
   import { PlusOutlined, CloudUploadOutlined, RedoOutlined, SearchOutlined } from "@ant-design/icons-vue"
   import ConfigForm from "./configForm.vue"
   import ImportForm from "./importForm.vue"
   import previewCode from "./preview.vue"
-	import { message } from "ant-design-vue";
+    import { message } from "ant-design-vue";
   import STable from "@/components/STable/index.vue"
   import BatchDeleteButton from "@/components/BatchDeleteButton/index.vue";
 
-	const columns = [
+    const columns = [
     {
       title: '序号',
       dataIndex: 'index',
       align: 'center',
       width: 50,
     },
-		{
-			title: '表名称',
-			dataIndex: 'tableName',
+        {
+            title: '表名称',
+            dataIndex: 'tableName',
       align: 'center',
-			resizable: true,
-			width: 150
-		},
-		{
-			title: '表描述',
-			dataIndex: 'tableComment',
+            resizable: true,
+            width: 150
+        },
+        {
+            title: '表描述',
+            dataIndex: 'tableComment',
       align: 'center',
-			resizable: true,
-			width: 200
-		},
+            resizable: true,
+            width: 200
+        },
     {
       title: '实体类名',
       dataIndex: 'entityName',
@@ -109,69 +109,69 @@
       align: 'center',
       width: 160
     },
-		{
-			title: '更新时间',
-			dataIndex: 'updateTime',
-			align: 'center',
-			width: 160
-		},
-		{
-			title: '操作',
-			dataIndex: 'action',
-			align: 'center',
-			resizable: true,
+        {
+            title: '更新时间',
+            dataIndex: 'updateTime',
+            align: 'center',
+            width: 160
+        },
+        {
+            title: '操作',
+            dataIndex: 'action',
+            align: 'center',
+            resizable: true,
       width: 200,
-		}
-	]
-	const selectedRowKeys = ref([])
-	// 使用状态options（0正常 1停用）
-	const statusOptions = [
-		{ label: "正常", value: 0 },
-		{ label: "已停用", value: 1 }
-	]
-	// 列表选择配置
-	const options = {
-		alert: {
-			show: false,
-			clear: () => {
-				selectedRowKeys.value = ref([])
-			}
-		},
+        }
+    ]
+    const selectedRowKeys = ref([])
+    // 使用状态options（0正常 1停用）
+    const statusOptions = [
+        { label: "正常", value: 0 },
+        { label: "已停用", value: 1 }
+    ]
+    // 列表选择配置
+    const options = {
+        alert: {
+            show: false,
+            clear: () => {
+                selectedRowKeys.value = ref([])
+            }
+        },
     rowSelection: {
       onChange: (selectedRowKey, selectedRows) => {
         selectedRowKeys.value = selectedRowKey
       }
     }
-	}
-	// 定义tableDOM
-	const tableRef = ref()
+    }
+    // 定义tableDOM
+    const tableRef = ref()
   const importFormRef = ref()
   const configFormRef = ref()
   const previewRef = ref()
-	const toolConfig = { refresh: true, height: true, columnSetting: false, striped: false }
-	const searchFormRef = ref()
-	const searchFormData = ref({})
+    const toolConfig = { refresh: true, height: true, columnSetting: false, striped: false }
+    const searchFormRef = ref()
+    const searchFormData = ref({})
 
-	// 表格查询 返回 Promise 对象
-	const loadData = (parameter) => {
-		let param = Object.assign(parameter, searchFormData.value)
-		return codegenApi.configPage(param).then((res) => {
-			return res.data
-		})
-	}
-	// 重置
-	const reset = () => {
-		searchFormRef.value.resetFields()
-		tableRef.value.refresh(true)
-	}
-	// 删除
-	const deleteConfig = (record) => {
+    // 表格查询 返回 Promise 对象
+    const loadData = (parameter) => {
+        let param = Object.assign(parameter, searchFormData.value)
+        return codegenApi.configPage(param).then((res) => {
+            return res.data
+        })
+    }
+    // 重置
+    const reset = () => {
+        searchFormRef.value.resetFields()
+        tableRef.value.refresh(true)
+    }
+    // 删除
+    const deleteConfig = (record) => {
     let data = { ids: [record.id] }
     codegenApi.deleteConfig(data).then((res) => {
-			message.success(res.message)
-			tableRef.value.refresh(true)
-		})
-	}
+            message.success(res.message)
+            tableRef.value.refresh(true)
+        })
+    }
   // 批量删除
   const batchDelete = (record) => {
     let data = { ids: selectedRowKeys.value }
@@ -198,6 +198,6 @@
   }
    /** 直接后代选择器 **/
   .ant-form-inline > .ant-form-item {
-		margin-bottom: 12px !important;
-	}
+        margin-bottom: 12px !important;
+    }
 </style>
