5e67de10bf504c0915154466da74b5d59fa9b272
shisong <rgkjhshi@gmail.com>
2025-10-07 00:14:47 +0800

    Update index.vue

--- src/views/sys/resource/module/index.vue
+++ src/views/sys/resource/module/index.vue
@@ -1,15 +1,15 @@
 <template>
   <a-card size="small">
-    <a-form ref="searchFormRef" :model="searchFormData">
+    <a-form ref="queryFormRef" :model="queryFormData">
       <a-row :gutter="24">
         <a-col :span="8">
           <a-form-item name="searchKey" label="名称关键词">
-            <a-input v-model:value="searchFormData.searchKey" placeholder="请输入关键词" allowClear />
+            <a-input v-model:value="queryFormData.searchKey" placeholder="请输入关键词" allowClear />
           </a-form-item>
         </a-col>
         <a-col :span="6">
           <a-form-item name="status" label="使用状态">
-            <a-select v-model:value="searchFormData.status" placeholder="请选择状态" :options="statusOptions" allowClear />
+            <a-select v-model:value="queryFormData.status" placeholder="请选择状态" :options="statusOptions" allowClear />
           </a-form-item>
         </a-col>
         <a-col :span="8">
@@ -89,10 +89,10 @@
   import STable from "@/components/STable/index.vue"
 
   // resourceType=1标识模块
-  const searchFormData = ref({ resourceType: 1 })
+  const queryFormData = ref({ resourceType: 1 })
   const addFormRef = ref()
   const editFormRef = ref()
-  const searchFormRef = ref()
+  const queryFormRef = ref()
   const tableRef = ref()
   const toolConfig = { refresh: true, height: true, columnSetting: false, striped: false }
   const columns = [
@@ -163,13 +163,13 @@
     }
   }
   const loadData = (parameter) => {
-    return resourceApi.resourcePage(Object.assign(parameter, searchFormData.value)).then((res) => {
+    return resourceApi.resourcePage(Object.assign(parameter, queryFormData.value)).then((res) => {
       return res.data
     })
   }
   // 重置
   const reset = () => {
-    searchFormRef.value.resetFields()
+    queryFormRef.value.resetFields()
     tableRef.value.refresh(true)
   }
   // 删除
