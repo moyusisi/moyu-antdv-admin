cee8c63baa5a96a1d3dac240f60df608aef2d93c
shisong <rgkjhshi@gmail.com>
2025-10-28 19:34:22 +0800

    Update menu.ts

--- src/store/menu.ts
+++ src/store/menu.ts
@@ -10,138 +10,138 @@ import Empty from '@/layout/empty.vue'
 
 // login和findPwd路由组件已静态加载，此处不在进行异步加载
 const modules = import.meta.glob([
-	'/src/views/**/**.vue',
-	'!/src/views/auth/findPwd/**.vue',
-	'!/src/views/auth/login/**.vue'
+  '/src/views/**/**.vue',
+  '!/src/views/auth/findPwd/**.vue',
+  '!/src/views/auth/login/**.vue'
 ])
 
-// 过滤异步路由
-const filterAsyncRoutes = (menus) => {
-	const asyncRoutes: RouteRecordRaw[] = [];
-	// 遍历所有菜单
-	menus.forEach((menu) => {
-		menu.meta = menu.meta ? menu.meta : {}
-		// 处理外部链接特殊路由
-		if (menu.meta.type === 'iframe') {
-			menu.meta.url = menu.path
-			menu.path = `/${menu.code}`
-		}
-		// menu转路由对象
-		const route: RouteRecordRaw = {
-			path: menu.path,
-			name: menu.code,
-			meta: menu.meta,
-			redirect: menu.redirect,
-			children: menu.children ? filterAsyncRoutes(menu.children) : null,
-			component: loadComponent(menu)
-		}
-		asyncRoutes.push(route);
-	});
-	return asyncRoutes;
-}
-
-// 加载组件
-const loadComponent = (menu) => {
-	// 资源类型（字典 1模块 2目录 3菜单 4内链 5外链 6按钮）
-	let item = Empty;
-	const component = menu.component
-	if (component?.toString() === "Layout") {
-		item = Layout
-	} else {
-		item = modules[`/src/views/${component}.vue`] || modules[`/src/views/${component}/index.vue`] || Empty
-	}
-	return item
-}
-
 export const useMenuStore = defineStore('menuStore', () => {
-	// state
-	// 所有路由(静态路由 + 动态路由)
-	const routes = ref<RouteRecordRaw[]>([]);
-	// 所有模块的menu,用于保存后端返回的原始数据
-	const menuList = ref([])
+  // state
+  // 所有路由(静态路由 + 动态路由)
+  const routes = ref<RouteRecordRaw[]>([]);
+  // 所有模块的menu,用于保存后端返回的原始数据
+  const menuList = ref([])
 
-	// 在 Setup Stores 中，您需要创建自己的 $reset() 方法 https://pinia.vuejs.org/zh/core-concepts/state.html
-	function $reset() {
-		menuList.value = []
-		routes.value = []
-	}
+  // 在 Setup Stores 中，您需要创建自己的 $reset() 方法 https://pinia.vuejs.org/zh/core-concepts/state.html
+  function $reset() {
+    menuList.value = []
+    routes.value = []
+  }
 
-	// actions
-	function setRoutes(newRoutes) {
-		routes.value = constRoutes.concat(newRoutes);
-	}
+  // actions
+  function setRoutes(newRoutes) {
+    routes.value = constRoutes.concat(newRoutes);
+  }
 
-	/**
-	 * 初始化模块及菜单(优先本地，无则api获取)
-	 */
-	const initModuleMenu = async () => {
-		// 优先获取本地数据
-		const menu = localStorage.getItem('MENU')
-		let localMenu = JSON.parse(menu as string)
-		if (localMenu) {
-			menuList.value = localMenu
-		} else {
-			// 本地无则从api获取
-			await refreshModuleMenu()
-		}
-	};
+  /**
+   * 初始化模块及菜单(优先本地，无则api获取)
+   */
+  const initModuleMenu = async () => {
+    // 优先获取本地数据
+    const menu = localStorage.getItem('MENU')
+    let localMenu = JSON.parse(menu as string)
+    if (localMenu) {
+      menuList.value = localMenu
+    } else {
+      // 本地无则从api获取
+      await refreshModuleMenu()
+    }
+  };
 
-	/**
-	 * 从api更新模块及菜单
-	 */
-	const refreshModuleMenu = async () => {
-		const res = await userCenterApi.loginUserMenu()
-		localStorage.setItem('MENU', JSON.stringify(res.data))
-		menuList.value = res.data
-	};
+  /**
+   * 从api更新模块及菜单
+   */
+  const refreshModuleMenu = async () => {
+    const res = await userCenterApi.loginUserMenu()
+    localStorage.setItem('MENU', JSON.stringify(res.data))
+    menuList.value = res.data
+  };
 
-	/**
-	 * 生成异步(动态)路由
-	 */
-	const generateRoutes = async () => {
-		// 生成动态路由
-		const asyncRoutes = filterAsyncRoutes(menuList.value);
-		// 设置routes
-		setRoutes(asyncRoutes)
-		// 初始化面包屑
-		initBreadcrumb(routes.value)
-		// 初始化搜索
-		const searchStore = useSearchStore()
-		searchStore.init(routes.value)
-		return asyncRoutes;
-	};
+  /**
+   * 生成异步(动态)路由
+   */
+  const generateRoutes = async () => {
+    // 生成动态路由
+    const asyncRoutes = parseAsyncRoutes(menuList.value);
+    // 设置routes
+    setRoutes(asyncRoutes)
+    // 初始化面包屑
+    initBreadcrumb(routes.value)
+    // 初始化搜索
+    const searchStore = useSearchStore()
+    searchStore.init(routes.value)
+    return asyncRoutes;
+  };
 
-	/**
-	 * 重新加载菜单及路由
-	 */
-	const reloadRoutes = async () => {
-		await refreshModuleMenu()
-		return await generateRoutes()
-	};
+  // 转换为路由对象
+  const parseAsyncRoutes = (menus) => {
+    const asyncRoutes: RouteRecordRaw[] = [];
+    // 遍历所有菜单
+    menus.forEach((menu) => {
+      menu.meta = menu.meta ? menu.meta : {}
+      // 处理外部链接特殊路由
+      if (menu.meta.type === 'iframe') {
+        menu.meta.url = menu.path
+        menu.path = `/${menu.code}`
+      }
+      // menu转路由对象
+      const route: RouteRecordRaw = {
+        name: menu.code,
+        path: menu.path,
+        meta: menu.meta,
+        redirect: menu.redirect,
+        children: menu.children && menu.children.length > 0 ? parseAsyncRoutes(menu.children) : undefined,
+        component: loadComponent(menu)
+      }
+      asyncRoutes.push(route);
+    });
+    return asyncRoutes;
+  }
 
-	/**
-	 * 根据当前的routes生成菜单的面包屑
-	 */
-	const initBreadcrumb = (routes: RouteRecordRaw[], breadcrumb: RouteRecordRaw[] = []) => {
-		routes.forEach((route) => {
-			// 生成当前route的breadcrumb
-			const temp: RouteRecordRaw[] = [...breadcrumb]
-			temp.push(route)
-			// 设置在route的meta中
-			route.meta.breadcrumb = temp
-			// 若有子路由则递归设置子路由
-			if (route.children) {
-				initBreadcrumb(route.children, temp)
-			}
-		})
-	}
+  // 加载组件
+  const loadComponent = (menu) => {
+    // 资源类型（字典 1模块 2目录 3菜单 4内链 5外链）
+    let item = Empty;
+    const component = menu.component
+    if (component?.toString() === "Layout") {
+      item = Layout
+    } else {
+      item = modules[`/src/views/${component}.vue`] || modules[`/src/views/${component}/index.vue`] || Empty
+    }
+    return item
+  }
 
-	return {
-		routes,
-		$reset,
-		initModuleMenu,
-		refreshModuleMenu,
-		generateRoutes,
-		reloadRoutes
-	}
+  /**
+   * 重新加载菜单及路由
+   */
+  const reloadRoutes = async () => {
+    await refreshModuleMenu()
+    return await generateRoutes()
+  };
+
+  /**
+   * 根据当前的routes生成菜单的面包屑
+   */
+  const initBreadcrumb = (routes: RouteRecordRaw[], breadcrumb: RouteRecordRaw[] = []) => {
+    routes.forEach((route) => {
+      // 生成当前route的breadcrumb
+      const temp: RouteRecordRaw[] = [...breadcrumb]
+      temp.push(route)
+      // 设置在route的meta中
+      route.meta.breadcrumb = temp
+      // 若有子路由则递归设置子路由
+      if (route.children) {
+        initBreadcrumb(route.children, temp)
+      }
+    })
+  }
+
+  return {
+    routes,
+    $reset,
+    initModuleMenu,
+    refreshModuleMenu,
+    generateRoutes,
+    reloadRoutes
+  }
 })
