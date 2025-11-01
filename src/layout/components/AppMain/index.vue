ee6debda191e7b368a3c066badfa6bb478e8fcc8
shisong <rgkjhshi@gmail.com>
2025-11-01 18:32:32 +0800

    最大缓存视图处理

--- src/layout/components/AppMain/index.vue
+++ src/layout/components/AppMain/index.vue
@@ -14,8 +14,10 @@ import IframeView from "./iframeView.vue"
 import { useTagsViewStore } from "@/store"
 const Error404  = () => import('@/views/other/404.vue')
 
+const tagsViewStore = useTagsViewStore()
+
 // 缓存页面集合, 直接解构store中的同名字段
-const { cachedViews } = toRefs(useTagsViewStore());
+const { cachedViews } = toRefs(tagsViewStore);
 
 // 当前组件 keep-alive通过组件的name匹配,而不是route的name
 const wrapperMap = new Map();
@@ -40,11 +42,13 @@ const currentComponent = (component, route) => {
     wrapperMap.set(componentName, wrapper);
   }
 
-  // 添加组件数量限制
+  // 添加缓存组件的数量限制
   if (wrapperMap.size > 10) {
     const firstKey = wrapperMap.keys().next().value;
     if (firstKey) {
       wrapperMap.delete(firstKey);
+      // 同时移除cachedViews列表中的视图
+      tagsViewStore.removeCachedView({ fullPath: firstKey })
     }
   }
   return h(wrapper);
