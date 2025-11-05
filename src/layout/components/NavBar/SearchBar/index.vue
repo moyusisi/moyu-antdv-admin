0ee11e9301271454d88bb3a684d02e39fbffc7a7
信息港华为云/shisong <rgkjhshi@gmail.com>
2025-11-05 20:06:18 +0800

    search

---
+++ src/layout/components/NavBar/SearchBar/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- 系统设置Bar -->
+  <div class="bar-item" @click="openSearch">
+    <SearchOutlined />
+  </div>
+  <!-- 系统设置抽屉 -->
+  <a-drawer v-model:open="searchOpen" :closable="false" width="500">
+    <Search />
+  </a-drawer>
+</template>
+
+<script setup lang="ts">
+import { SearchOutlined } from "@ant-design/icons-vue";
+import Search from "@/layout/components/NavBar/SearchBar/search.vue"
+
+const searchOpen = ref(false)
+
+// 搜索面板
+const openSearch = () => {
+  searchOpen.value = true
+}
+</script>
+
+<style scoped>
+
+</style>
