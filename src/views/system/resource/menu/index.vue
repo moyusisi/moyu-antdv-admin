74f1f3035390217de3efaac32377b8ef006804fa
shisong <rgkjhshi@gmail.com>
2025-12-27 20:11:42 +0800

    菜单列表中不再显示有无数据权限

--- src/views/system/resource/menu/index.vue
+++ src/views/system/resource/menu/index.vue
@@ -72,14 +72,11 @@
           <span v-else />
         </template>
         <template v-if="column.dataIndex === 'visible'">
-          <a-tooltip v-if="node.resourceType === 6" title="数据权限">
-            <a-tag v-if="node.visible === 1" color="blue">有</a-tag>
-            <a-tag v-else>无</a-tag>
-          </a-tooltip>
-          <span v-else >
+          <span v-if="node.resourceType !== 6" >
             <a-tag v-if="node.visible === 1" color="green">可见</a-tag>
             <a-tag v-else>隐藏</a-tag>
           </span>
+          <span v-else ></span>
         </template>
         <template v-if="column.dataIndex === 'action'">
           <a-space>
