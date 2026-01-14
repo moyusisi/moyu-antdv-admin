8a047407781611d687237bb9c95a3b6b8db985b1
shisong <rgkjhshi@gmail.com>
2026-01-14 13:39:34 +0800

    Update index.vue

--- src/views/system/components/UserSelector/index.vue
+++ src/views/system/components/UserSelector/index.vue
@@ -1,11 +1,9 @@
 <template>
   <a-drawer
       v-model:open="visible"
-      :open="visible"
       title="用户选择"
       :width="drawerWidth"
       :closable="false"
-      :footerStyle="{display: 'flex', justifyContent: 'flex-end'}"
       :destroy-on-close="true"
       @close="onClose"
   >
@@ -15,16 +13,16 @@
     <!-- 页面内容 -->
     <a-row :gutter="8">
       <!-- 左侧组织树 -->
-      <a-col :span="5">
+      <a-col :span="4">
         <OrgTree ref="treeRef" @onSelect="treeSelect"></OrgTree>
       </a-col>
-      <a-col :span="19">
+      <a-col :span="10">
         <a-card size="small">
           <a-form ref="searchFormRef" :model="searchFormData">
             <a-row :gutter="16">
               <a-col :span="8">
                 <a-form-item name="searchKey">
-                  <a-input v-model:value="searchFormData.searchKey" placeholder="请输入用户名" allowClear />
+                  <a-input v-model:value="searchFormData.searchKey" placeholder="搜索用户名" allowClear />
                 </a-form-item>
               </a-col>
               <a-col :span="8">
@@ -34,7 +32,7 @@
                 </a-space>
               </a-col>
               <a-col :span="8" style="text-align: right">
-                <a-button type="dashed" @click="addRows" :icon="h(PlusOutlined)" style="color: #52C41AFF; border-color: #52C41AFF">添加</a-button>
+                <a-button @click="addRows" style="color: #52C41AFF; border-color: #52C41AFF">添加当前数据</a-button>
               </a-col>
             </a-row>
           </a-form>
@@ -43,21 +41,63 @@
                :columns="columns"
                :data-source="tableData"
                :row-key="(record) => record.account"
-               :row-selection="rowSelection"
                :pagination="paginationRef"
                @change="handleTableChange"
                bordered>
-            <template #bodyCell="{ column, record }">
-              <template v-if="column.dataIndex === 'gender'">
-                <a-tag v-if="record.gender === 1" color="blue">男</a-tag>
-                <a-tag v-else-if="record.gender === 2" color="pink">女</a-tag>
-                <a-tag v-else>未知</a-tag>
-              </template>
-              <template v-if="column.dataIndex === 'status'">
-                <a-tag v-if="record.status === 0" color="green">正常</a-tag>
-                <a-tag v-else>已停用</a-tag>
-              </template>
+            <template #bodyCell="{ column, record, index, text }">
               <template v-if="column.dataIndex === 'action'">
+                <a-button type="dashed" size="small" @click="addRows(record)"><PlusOutlined /></a-button>
+              </template>
+              <template v-if="column.dataIndex === 'account'">
+                <!-- 长文本省略提示 -->
+                <a-tooltip :title="text" placement="topLeft">
+                  <span>{{ text }}</span>
+                </a-tooltip>
+              </template>
+              <template v-if="column.dataIndex === 'name'">
+                <!-- 长文本省略提示 -->
+                <a-tooltip :title="text" placement="topLeft">
+                  <span>{{ text }}</span>
+                </a-tooltip>
+              </template>
+            </template>
+          </a-table>
+        </a-card>
+      </a-col>
+      <a-col :span="10">
+        <a-card size="small">
+          <a-form ref="searchFormRef" :model="searchFormData">
+            <a-row :gutter="16">
+              <a-col :span="24" style="text-align: right">
+                <a-form-item>
+                  <a-button danger @click="delRows">全部清除</a-button>
+                </a-form-item>
+              </a-col>
+            </a-row>
+          </a-form>
+          <a-table size="small"
+                   ref="tableRef"
+                   :columns="columns"
+                   :data-source="selectTableData"
+                   :row-key="(record) => record.account"
+                   :pagination="paginationRef"
+                   @change="handleTableChange"
+                   bordered>
+            <template #bodyCell="{ column, record, index, text }">
+              <template v-if="column.dataIndex === 'action'">
+                <a-button type="dashed" danger size="small" @click="delRows(record)"><MinusOutlined /></a-button>
+              </template>
+              <template v-if="column.dataIndex === 'account'">
+                <!-- 长文本省略提示 -->
+                <a-tooltip :title="text" placement="topLeft">
+                  <span>{{ text }}</span>
+                </a-tooltip>
+              </template>
+              <template v-if="column.dataIndex === 'name'">
+                <!-- 长文本省略提示 -->
+                <a-tooltip :title="text" placement="topLeft">
+                  <span>{{ text }}</span>
+                </a-tooltip>
               </template>
             </template>
           </a-table>
@@ -66,9 +106,9 @@
     </a-row>
     <!-- 底部内容 -->
     <template #footer>
-      <a-space>
-        <a-button @click="onClose">关闭</a-button>
-      </a-space>
+      <a-flex gap="small" justify="flex-end">
+        <a-button type="primary" danger @click="onClose"> 关闭</a-button>
+      </a-flex>
     </template>
   </a-drawer>
 </template>
@@ -87,45 +127,24 @@
 
   const columns = [
     {
+      title: '操作',
+      dataIndex: 'action',
+      align: 'center',
+      width: 50
+    },
+    {
       title: '姓名',
       dataIndex: 'name',
       align: 'center',
-      resizable: true,
-      width: 100
+      width: 200
     },
     {
       title: '账号',
       dataIndex: 'account',
       align: 'center',
-      resizable: true,
-      width: 100,
-      ellipsis: true
-    },
-    {
-      title: '性别',
-      dataIndex: 'gender',
-      align: 'center',
-      width: 80
-    },
-    {
-      title: '组织机构',
-      dataIndex: 'orgName',
-      resizable: true,
       width: 200,
       ellipsis: true
     },
-    {
-      title: '手机',
-      dataIndex: 'phone',
-      align: 'center',
-      width: 150
-    },
-    {
-      title: '状态',
-      dataIndex: 'status',
-      align: 'center',
-      width: 80
-    }
   ]
 
   // 默认是关闭状态
@@ -139,19 +158,11 @@
   const searchFormData = ref({})
   // table数据
   const tableRef = ref()
-  // 表格中的数据(loadTableData中会更新)
+  // 表格中的数据(loadData中会更新)
   const tableData = ref([])
-  // 已选中的菜单(loadTableData中会更新)
+  const selectTableData = ref([])
+  // 已选中的菜单
   const selectedRowKeys = ref([])
-  // 列表选择配置
-  const rowSelection = ref({
-    checkStrictly: false,
-    selectedRowKeys: selectedRowKeys,
-    onChange: (selectedKeys, selectedRows) => {
-      selectedRowKeys.value = selectedKeys
-      // console.log('onChange,selectedKeys:', selectedKeys);
-    }
-  });
   // 表格的分页配置
   const paginationRef = ref({
     // 当前页码
@@ -218,18 +229,45 @@
     loadData()
   }
   // 添加记录
-  const addRows = () => {
-    if (selectedRowKeys.value.length < 1) {
-      message.warning('请选择一条或多条数据')
-      return
+  const addRows = (record) => {
+    if (record && record.account) {
+      // 添加单行
+      const selectedRecord = selectTableData.value.filter((item) => item.account === record.account)
+      if (selectedRecord.length === 0) {
+        selectTableData.value.push(record)
+      } else {
+        message.warning('该记录已存在')
+      }
+    } else {
+      // 添加本页数据
+      let newArray = []
+      tableData.value.forEach((row) => {
+        const index = selectTableData.value.findIndex(item => item.account === row.account);
+        if (index === -1) {
+          newArray.push(row)
+        }
+      })
+      if (newArray.length === 0) {
+        message.warning('这些记录已存在')
+      } else {
+        selectTableData.value.push(...newArray)
+      }
     }
-    let data = { code: role.value.code, codeSet: selectedRowKeys.value }
-    roleApi.roleAddUser(data).then((res) => {
-      message.success(res.message)
-      emit('successful')
-      // 添加之后重新加载数据
-      loadData()
-    })
+  }
+  // 删减记录
+  const delRows = (record) => {
+    if (record && record.account) {
+      // 删除单行
+      // 1. 按唯一属性找索引
+      const index = selectTableData.value.findIndex(item => item.account === record.account);
+      // 2. 删除该对象
+      if (index !== -1) {
+        selectTableData.value.splice(index, 1);
+      }
+    } else {
+      // 全删除
+      selectTableData.value.length = 0
+    }
   }
   // 调用这个函数将子组件的一些数据和方法暴露出去
   defineExpose({
