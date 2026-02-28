f7de43ba87fda2bfe48eb84d22e7d0fd1331ef78
shisong <rgkjhshi@gmail.com>
2026-03-01 03:20:19 +0800

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -54,7 +54,7 @@ router.beforeEach(async (to, from) => {
   // fullPath是包括 路径、查询参数和哈希值的完整地址。
   console.log("访问地址: " + to.fullPath, to)
   // 如果未加载用户信息，则先加载用户信息
-  if (!userStore.userInfo.account) {
+  if (!userStore.userInfo?.account) {
     console.log("加载userInfo...")
     await userStore.initUserInfo();
   }
