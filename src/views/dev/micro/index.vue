874dcfb3550468eea3be781b17384ec0d9fc0385
shisong <rgkjhshi@gmail.com>
2026-03-04 19:33:28 +0800

    Update index.vue

--- src/views/dev/micro/index.vue
+++ src/views/dev/micro/index.vue
@@ -7,6 +7,7 @@
 
 <script setup>
 import { useRoute } from "vue-router";
+import WujieVue from "wujie-vue3";
 import { microMap } from "@/microApp.ts";
 
 // 获取当前路由对象（route 是响应式的，路由变化时会自动更新）
@@ -16,17 +17,17 @@ let subAppName = ref("subApp");
 
 // 在mount之前初始化无界所需参数(解决子应用重复mount问题)
 onBeforeMount(() => {
-  console.log('主应用挂载容器前初始化无界参数')
+  console.log('主应用onBeforeMount挂载容器前初始化无界参数')
   // 通过 fullPath 自动匹配子应用
   let fullPath = route.fullPath;
   for (const appName of Object.keys(microMap)) {
     if (fullPath.startsWith(`/${appName}`)) {
-      subAppName = appName;
+      subAppName.value = appName;
       break;
     }
   }
-  fullPath = fullPath.replace(`/${subAppName}`, '');
-  let host = microMap[subAppName]
+  fullPath = fullPath.replace(`/${subAppName.value}`, '');
+  let host = microMap[subAppName.value]
   // 兼容host结尾包含'/'于fullPath重复的情况
   if (host.endsWith('/')) {
     host = host.slice(0, -1)
