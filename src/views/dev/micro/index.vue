697846f52ff85d74488d70bb6fa2a44ea870bf6f
shisong <rgkjhshi@gmail.com>
2026-03-31 19:33:28 +0800

    micro/index

--- src/views/dev/micro/index.vue
+++ src/views/dev/micro/index.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="admin-ui-main">
     <!--保活模式，name相同则复用一个子应用实例，改变url无效，必须采用通信的方式告知路由变化 -->
-    <WujieVue width="100%" height="100%" :name="subAppName" :url="subAppUrl" :props="props" :sync="false"></WujieVue>
+    <WujieVue width="100%" height="100%" :name="subAppName" :url="subAppUrl" :props="props" :sync="false"
+              :beforeLoad="beforeLoad" :beforeMount="beforeMount"></WujieVue>
+    <a-spin :spinning="subLoading" tip="子应用加载中..." size="large" v-if="subLoading" style="background-color: #f6f8f9" />
   </div>
 </template>
 
@@ -53,6 +55,16 @@ function initProps() {
     "token": localStorage.getItem('TOKEN'),
     "userInfo": JSON.parse(localStorage.getItem('USER_INFO')),
   }
+}
+
+// 子应用生命周期钩子函数
+function beforeLoad() {
+  console.log(`${subAppName.value} beforeLoad 生命周期`)
+  subLoading.value = true;
+}
+function beforeMount() {
+  subLoading.value = false;
+  console.log(`${subAppName.value} beforeMount 生命周期`)
 }
 
 </script>
