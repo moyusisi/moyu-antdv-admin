216c8bd9d997a59076962f1c9402136203bec4ba
shisong <rgkjhshi@gmail.com>
2026-07-27 19:06:06 +0800

    Update systemRouter.ts

--- src/router/systemRouter.ts
+++ src/router/systemRouter.ts
@@ -10,7 +10,7 @@ import Layout from '@/layout/index.vue'
  *
  * * meta.title      标题名称
  * * meta.icon       图标
- * * meta.type       菜单类型 dir|menu|iframe|link 等
+ * * meta.type       菜单类型 dir|menu|link 等
  * * meta.hidden     是否隐藏
  * * meta.brief     【目录】是否简洁模式(目录下只有一个菜单时，不显示目录直接显示该菜单)
  * * meta.affix     【菜单】是否固定显示(标签页)
