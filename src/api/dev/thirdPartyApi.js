3b636a3c25eaf6c6cee9bb31b6ee5c546f533844
shisong <rgkjhshi@gmail.com>
2026-08-17 16:51:31 +0800

    Update thirdPartyApi.js

--- src/api/dev/thirdPartyApi.js
+++ src/api/dev/thirdPartyApi.js
@@ -6,28 +6,28 @@ import service from '@/utils/request'
 export default {
   // 分页查询三方集成接口列表
   thirdPartyApiPage(data) {
-    return service.postJson('/api/dev/thirdPartyApi/page', data)
+    return service.postJson('/api/dev/thirdApi/page', data)
   },
   // 获取三方集成接口详情
   thirdPartyApiDetail(data) {
-    return service.postJson('/api/dev/thirdPartyApi/detail', data)
+    return service.postJson('/api/dev/thirdApi/detail', data)
   },
   // 新增三方集成接口
   addThirdPartyApi(data) {
-    return service.postJson('/api/dev/thirdPartyApi/add', data)
+    return service.postJson('/api/dev/thirdApi/add', data)
   },
   // 编辑三方集成接口
   editThirdPartyApi(data) {
-    return service.postJson('/api/dev/thirdPartyApi/edit', data)
+    return service.postJson('/api/dev/thirdApi/edit', data)
   },
   // 删除三方集成接口，通过ids删除
   deleteThirdPartyApi(data) {
-    return service.postJson('/api/dev/thirdPartyApi/delete', data)
+    return service.postJson('/api/dev/thirdApi/delete', data)
   },
 
   // 调试接口
   debugApi(data) {
-    return service.postJson('/api/dev/thirdPartyApi/debugApi', data)
+    return service.postJson('/api/dev/thirdApi/debugApi', data)
   }
 
 }
