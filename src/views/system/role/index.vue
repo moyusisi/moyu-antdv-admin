41b42c499f6b05fa0cfc3339303d8c9cedb4d49b
shisong <rgkjhshi@gmail.com>
2026-08-28 12:39:19 +0800

    Update index.vue

--- src/views/system/role/index.vue
+++ src/views/system/role/index.vue
@@ -135,7 +135,7 @@
       { field: 'sortNum', title: '排序顺序', width: 100 },
       { field: 'remark', title: '备注' },
       { field: 'updateTime', title: '更新时间', width: 170 },
-      { field: 'action', title: '操作', width: 200, slots: { default: 'action' } },
+      { field: 'action', title: '操作', width: 250, slots: { default: 'action' } },
     ],
     // 工具栏配置
     toolbarConfig: {
